aa62f47622c93cf211232ae9f182c2b916a6b461
Jonathan Jesneck <jonathan.jesneck@gmail.com>
2020-02-24 10:40:59 -0600

    random number to guess

--- guess.py
+++ guess.py
@@ -1,6 +1,10 @@
+import random
+
+
 def play_game():
-    num = 3
-    guess = input('Make your guess')
+    num = random.randrange(1, 10)
+    print('Please guess from 1 to 10')
+    guess = input('Make your guess: ')
     guess = int(guess)
     if guess == num:
         print('YOU WINN!!')
