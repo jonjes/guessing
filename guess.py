5c4192dd29b6fa46fc8f4922fb2fb135d7b6fc89
Jonathan Jesneck <jonathan.jesneck@gmail.com>
2020-02-24 10:44:12 -0600

    validate input

--- guess.py
+++ guess.py
@@ -1,6 +1,9 @@
 def play_game():
     num = 3
     guess = input('Make your guess')
+    if not guess.isdigit():
+        print('Please enter an integer')
+        exit()
     guess = int(guess)
     if guess == num:
         print('YOU WINN!!')
